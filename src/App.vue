eefcb77a4165456c758d18b29180cbacdd2a275d
Magnus Meseck <magnus.meseck@googlemail.com>
2023-04-07 21:30:43 +0200

    Initial setup

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <HelloWorld msg="I am here!"/>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-facing-decorator';
+import HelloWorld from './components/HelloWorld.vue';
+
+@Component({
+  components: {
+    HelloWorld
+  }})
+export default class App extends Vue 
+{
+
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 160px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="container">
+        <h1>{{ msg }}</h1>
+
+        <p>
+            Recommended IDE setup:
+            <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
+            +
+            <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
+        </p>
+
+        <p>See <code>README.md</code> for more information.</p>
+
+        <p>
+            <a href="https://vitejs.dev/guide/features.html" target="_blank">
+                Vite Docs
+            </a>
+            |
+            <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
+        </p>
+
+        <button type="button" @click="increment()"> Counter is at: {{ count }}</button>
+        <p>
+            Change
+            <code>components/HelloWorld.vue</code> to test hot module replacement.
+        </p>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-facing-decorator';
+
+@Component({ components: {  } })
+export default class HelloWorld extends Vue 
+{
+    @Prop({default: "Default message!"}) 
+    msg!: string;
+    
+    count = 0;
+    
+    increment()
+    {
+        this.count++;
+    }
+}
+</script>
+
+<style scoped>
+a {
+    color: #42b983;
+}
+
+label {
+    margin: 0 0.5em;
+    font-weight: bold;
+}
+
+code {
+    background-color: #eee;
+    padding: 2px 4px;
+    border-radius: 4px;
+    color: #304455;
+}
+
+.container {
+    width: 600px;
+    margin: auto;
+}
+</style>
